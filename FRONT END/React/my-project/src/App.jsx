a7f4d288a8f49462ea09e8e5e99679fcfdb05578
ADITYA IMAM ZUHDI <100418056+adityaimamz@users.noreply.github.com>
2023-09-06 10:12:57 +0700

    Learn React Componen, props and childern

--- FRONT END/React/my-project/src/App.jsx
+++ FRONT END/React/my-project/src/App.jsx
@@ -1,9 +1,28 @@
+
+const Button = (props) => {
+  //destructuring
+  // eslint-disable-next-line react/prop-types
+  const { variant = "bg-sky-600" , children = "..." } = props;
+
+  return (
+    <button
+      className={`h-10 px-6 font-semibold rounded-md ${variant} text-white`}
+    >
+      {children}
+    </button>
+  );
+};
+
 function App() {
   return (
-    <div className="flex justify-center bg-blue-600 min-h-screen items-center">
-      <div>Hello CodeSandbox</div>
+    <div className="flex justify-center bg-blue-600 min-h-screen font-bold items-center">
+      <div className="flex gap-4">
+        <Button variant="bg-red-600">Register</Button>
+        <Button variant="bg-slate-500">Login</Button>
+        <Button ></Button>
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
