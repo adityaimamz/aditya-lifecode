72ad911c86796c8d304208c5c85218a0005d3446
ADITYA IMAM ZUHDI <100418056+adityaimamz@users.noreply.github.com>
2023-09-06 21:28:58 +0700

    Learn React 8.2 Controller

--- BACK END/REST API/src/controllers/product.controller.ts
+++ BACK END/REST API/src/controllers/product.controller.ts
@@ -1,4 +1,7 @@
-import { Router, type Request, type Response } from 'express'
+/* eslint-disable array-callback-return */
+/* eslint-disable @typescript-eslint/strict-boolean-expressions */
+/* eslint-disable @typescript-eslint/explicit-function-return-type */
+import { type Request, type Response } from 'express'
 import { logger } from '../utils/logger'
 import { createProductValidation } from '../validations/product.validation'
 
@@ -10,4 +13,36 @@ export const createProduct = (req: Request, res: Response) => {
   }
   logger.info('Succes add new product data')
   return res.status(200).send({ status: true, statusCode: '200', message: 'Succes add new product', data: value })
+}
+export const getProduct = (req: Request, res: Response) => {
+  const products = [
+    {
+      name: 'Sepatu Sport',
+      price: 50000
+    },
+    {
+      name: 'Sepatu Casual',
+      price: 20000
+    }
+  ]
+  const {
+    params: { name }
+  } = req
+
+  if (name) {
+    const filterProduct = products.filter((product) => {
+      if (product.name === name) {
+        return product
+      }
+    })
+    if (filterProduct.length === 0) {
+      logger.info('Data Not Found')
+      res.status(404).send({ status: false, statusCode: 404, data: {} })
+    }
+    logger.info('Succes get product data')
+    res.status(200).send({ status: true, statusCode: 200, data: filterProduct[0] })
+  }
+
+  logger.info('Succes get product data')
+  res.status(200).send({ status: true, statusCode: 200, data: products })
 }
--- BACK END/REST API/src/routes/product.routes.ts
+++ BACK END/REST API/src/routes/product.routes.ts
@@ -1,12 +1,9 @@
 /* eslint-disable @typescript-eslint/strict-boolean-expressions */
-import { Router, type Request, type Response, type NextFunction } from 'express'
-import { logger } from '../utils/logger'
-import { createProduct } from '../controllers/product.controller'
+import { Router } from 'express'
+import { getProduct, createProduct } from '../controllers/product.controller'
 
 export const ProductRouter: Router = Router()
 
-ProductRouter.get('/', (req: Request, res: Response, next: NextFunction) => {
-  logger.info('Succes get product data')
-  res.status(200).send({ status: true, statusCode: '200', data: [{ name: 'Sepatu Sport', price: 50000 }] })
-})
+ProductRouter.get('/', getProduct)
+ProductRouter.get('/:name', getProduct)
 ProductRouter.post('/', createProduct)
