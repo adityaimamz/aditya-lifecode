b13858e9d674a388e768d06b29040656d74304bc
ADITYA IMAM ZUHDI <100418056+adityaimamz@users.noreply.github.com>
2023-09-06 14:41:34 +0700

    learn react 6. Router

--- FRONT END/React/my-project/src/Pages/register.jsx
+++ FRONT END/React/my-project/src/Pages/register.jsx
@@ -1,9 +1,11 @@
 import AuthLayout from "../components/layouts/AuthLayout";
 import FormRegister from "../components/Fragments/FormRegister";
+import { Link } from "react-router-dom";
 const RegisterPage = () => {
   return (
     <AuthLayout title="Register">
         <FormRegister></FormRegister>
+        <p className="text-sm mt-4 font-medium text-center">You already have an account ? <Link to="/login" className="text-blue-500">Login</Link></p>
     </AuthLayout>
   );
 };
--- FRONT END/React/my-project/src/components/layouts/AuthLayout.jsx
+++ FRONT END/React/my-project/src/components/layouts/AuthLayout.jsx
@@ -2,12 +2,14 @@
 const AuthLayout = (props) => {
     const { children, title } = props;
     return (
-      <div className="w-full max-w-xs">
-        <h1 className="text-blue-500 font-bold text-3xl mb-1">{title}</h1>
-        <p className="font-medium text-slate-500 mb-5">
-          Please Welcome Your Detail
-        </p>
-        {children}
+      <div className="flex justify-center min-h-screen font-bold items-center">
+        <div className="w-full max-w-xs">
+          <h1 className="text-blue-500 font-bold text-3xl mb-1">{title}</h1>
+          <p className="font-medium text-slate-500 mb-5">
+            Please Welcome Your Detail
+          </p>
+          {children}
+        </div>
       </div>
     );
 }
