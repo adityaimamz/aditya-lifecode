44cf4424ee5a0915cf61a8d25e368561b7511dfd
ADITYA IMAM ZUHDI <100418056+adityaimamz@users.noreply.github.com>
2023-09-07 13:18:11 +0700

    Learn React 7.Conditional Rendering

--- FRONT END/React/my-project/src/Pages/login.jsx
+++ FRONT END/React/my-project/src/Pages/login.jsx
@@ -1,18 +1,11 @@
 import AuthLayout from "../components/layouts/AuthLayout";
 import FormLogin from "../components/Fragments/FormLogin";
-import { Link } from "react-router-dom";
 const LoginPage = () => {
-    return (
-      <AuthLayout title="Login">
-        <FormLogin />
-        <p className="text-sm mt-4 font-medium text-center">
-          Dont have an account ?{" "}
-          <Link to="/register" className="text-blue-500">
-            Sign Up
-          </Link>
-        </p>
-      </AuthLayout>
-    );
-}
+  return (
+    <AuthLayout title="Login" type="login">
+      <FormLogin />
+    </AuthLayout>
+  );
+};
 
-export default LoginPage
+export default LoginPage;
--- FRONT END/React/my-project/src/components/layouts/AuthLayout.jsx
+++ FRONT END/React/my-project/src/components/layouts/AuthLayout.jsx
@@ -1,17 +1,57 @@
+import { Link } from "react-router-dom";
 /* eslint-disable react/prop-types */
 const AuthLayout = (props) => {
-    const { children, title } = props;
-    return (
-      <div className="flex justify-center min-h-screen font-bold items-center">
-        <div className="w-full max-w-xs">
-          <h1 className="text-blue-500 font-bold text-3xl mb-1">{title}</h1>
-          <p className="font-medium text-slate-500 mb-5">
-            Please Welcome Your Detail
-          </p>
-          {children}
-        </div>
+  const { children, title, type } = props;
+  return (
+    <div className="flex justify-center min-h-screen font-bold items-center">
+      <div className="w-full max-w-xs">
+        <h1 className="text-blue-500 font-bold text-3xl mb-1">{title}</h1>
+        <p className="font-medium text-slate-500 mb-5">
+          Please Welcome Your Detail
+        </p>
+        {children}
+        <Navigation type={type} />
       </div>
-    );
-}
+    </div>
+  );
+};
 
-export default AuthLayout
+ const Navigation = ({type}) => {
+   if (type === "login"){
+     return (
+       <p className="text-sm mt-4 font-medium text-center">
+         Don't have an account ?&nbsp; 
+         <Link to="/register" className="text-blue-500">
+           Sign Up
+         </Link>
+       </p>
+     );
+   } else {
+    return (
+      <p className="text-sm mt-4 font-medium text-center">
+        Already have an account ?{" "} 
+        <Link to="/login" className="text-blue-500">
+          Login
+        </Link>
+        </p>
+    )
+   }
+ }
+
+export default AuthLayout;
+
+  //  <p className="text-sm mt-4 font-medium text-center">
+  //    {type === "login"
+  //      ? "Don't have an account ? "
+  //      : "Already have an account ? "}
+  //    {type === "login" && (
+  //      <Link to="/register" className="text-blue-500">
+  //        Sign Up
+  //      </Link>
+  //    )}
+  //    {type === "register" && (
+  //      <Link to="/login" className="text-blue-500">
+  //        Login
+  //      </Link>
+  //    )}
+  //  </p>;
