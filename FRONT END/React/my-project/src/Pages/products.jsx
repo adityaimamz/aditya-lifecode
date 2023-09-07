cd41c3eeef6bf60b32fa4351b259b7ab18528984
ADITYA IMAM ZUHDI <100418056+adityaimamz@users.noreply.github.com>
2023-09-07 14:39:47 +0700

    learn React 9. Rendering List

--- FRONT END/React/my-project/src/Pages/products.jsx
+++ FRONT END/React/my-project/src/Pages/products.jsx
@@ -1,29 +1,46 @@
 import CardProduct from "../components/Fragments/CardProduct";
+
+const products = [
+  {
+    id: 1,
+    name: "Sepatu Baru",
+    price: "Rp. 100.000",
+    image: "/images/fashion-shoes-sneakers.jpg",
+    description:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Magni molestiae nisi enim libero quas harum, ratione iure consectetur id voluptates.",
+  },
+  {
+    id: 2,
+    name: "Tas Trendy",
+    price: "Rp. 150.000",
+    image: "/images/fashion-shoes-sneakers.jpg",
+    description:
+      "Tas trendi dengan desain modern yang cocok untuk segala kesempatan. Dibuat dari bahan berkualitas tinggi dan tahan lama.",
+  },
+  {
+    id: 3,
+    name: "Kacamata Fashion",
+    price: "Rp. 50.000",
+    image: "/images/fashion-shoes-sneakers.jpg",
+    description:
+      "Kacamata fashion dengan gaya yang keren. Melindungi mata Anda dari sinar matahari dan membuat Anda tampil stylish.",
+  },
+];
+
 const ProductPage = () => {
   return (
     <div className="flex justify-center pt-5">
-      <CardProduct>
-        <CardProduct.Header image="/images/fashion-shoes-sneakers.jpg"></CardProduct.Header>
-        <CardProduct.Body title="Sepatu Baru">
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Magni
-          molestiae nisi enim libero quas harum, ratione iure consectetur id
-          voluptates.
-        </CardProduct.Body>
-        <CardProduct.Footer price="Rp. 100.000"></CardProduct.Footer>
-      </CardProduct>
-      <CardProduct>
-        <CardProduct.Header image="/images/fashion-shoes-sneakers.jpg"></CardProduct.Header>
-        <CardProduct.Body title="Sepatu Baru">
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Magni
-          molestiae nisi enim libero quas harum, ratione iure consectetur id
-          voluptates.
-        </CardProduct.Body>
-        <CardProduct.Footer price="Rp. 100.000"></CardProduct.Footer>
-      </CardProduct>
+      {products.map((product) => (
+        <CardProduct key={product.id}>
+          <CardProduct.Header image={product.image} />
+          <CardProduct.Body title={product.name}>
+            {product.description}
+          </CardProduct.Body>
+          <CardProduct.Footer price={product.price} />
+        </CardProduct>
+      ))}
     </div>
   );
 };
-
-
 
 export default ProductPage;
