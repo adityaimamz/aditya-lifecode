3a49ac327af9c57bf561eef70cf6488f8d7cd356
ADITYA IMAM ZUHDI <100418056+adityaimamz@users.noreply.github.com>
2023-09-18 12:09:52 +0700

    learn Nextjs 1.routing

--- FRONT END/React/next js/src/pages/_app.tsx
+++ FRONT END/React/next js/src/pages/_app.tsx
@@ -1,5 +1,12 @@
+import Navbar from '@/component/layouts/Navbar'
 import type { AppProps } from 'next/app'
+import '@/styles/globals.css'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return(
+  <>
+    <Navbar/>
+      <Component {...pageProps} />
+  </>
+  )
 }
---
+++ FRONT END/React/next js/src/pages/product/[id].tsx
@@ -0,0 +1,10 @@
+const DetailProduct = () => {
+    return (
+        <div>
+            <h1>Detail Product</h1>
+            <p>Product : </p>
+        </div>
+    )
+}
+
+export default DetailProduct
