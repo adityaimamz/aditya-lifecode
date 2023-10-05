7c6d4e9255e74b174d70414886321ccc71e0aaa9
ADITYA IMAM ZUHDI <100418056+adityaimamz@users.noreply.github.com>
2023-10-05 21:11:10 +0700

    learn express middleware

--- BACK END/expressjs/app.js
+++ BACK END/expressjs/app.js
@@ -2,8 +2,17 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const CategoriesRouter = require('./routes/categories');
+const morgan = require('morgan');
 
-app.use(express.json());
+
+
+app.use(express.json())
+// app.use((req, res, next) => {
+//     req.requestTime = new Date().toISOString();
+//     next();
+// });
+
+app.use(morgan('dev'));
 
 app.use('/api/v1/categories', CategoriesRouter);
 
--- BACK END/expressjs/routes/categories.js
+++ BACK END/expressjs/routes/categories.js
@@ -1,13 +1,11 @@
 const express = require("express");
-
 const router = express.Router();
-
 const { getAllCategories, storeCategory } = require("../controllers/categoryController");
 
 //Routing
-router.get("/", getAllCategories);
+router.get("/", getAllCategories)
 
-router.post("/", storeCategory);
+router.post("/", storeCategory)
 
 router.get("/:nama", (req, res) => {
   res.send(`ini endpoint dari route params ${req.params.nama}`);
