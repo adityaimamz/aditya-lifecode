10c78d79085aef2208bd60890bfb2fbd21556dcb
ADITYA IMAM ZUHDI <100418056+adityaimamz@users.noreply.github.com>
2023-10-06 10:58:02 +0700

    Setup Sequelize-CLI ExpressJS

--- BACK END/expressjs/app.js
+++ BACK END/expressjs/app.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const dotenv = require('dotenv');
+const cors = require('cors');
+
 const CategoriesRouter = require('./routes/categories');
 const morgan = require('morgan');
 
+dotenv.config();
 
 
 app.use(express.json())
@@ -13,10 +16,11 @@ app.use(express.json())
 // });
 
 app.use(morgan('dev'));
+app.use(cors());
 
 app.use('/api/v1/categories', CategoriesRouter);
 
-
+const port = process.env.PORT || 5000;
 app.listen(port, () => {
     console.log(`Server berjalan di http://localhost:${port}`);
 })
