08e6d85e4692b8ea78d58a9bc69785101294879e
ADITYA IMAM ZUHDI <100418056+adityaimamz@users.noreply.github.com>
2023-10-09 14:45:31 +0700

    Permission User Role

--- BACK END/expressjs/middleware/UserMiddleware.js
+++ BACK END/expressjs/middleware/UserMiddleware.js
@@ -1,5 +1,5 @@
 const jwt = require("jsonwebtoken");
-const { User } = require("../models");
+const { User, Role } = require("../models");
 
 exports.authMiddleware = async (req, res, next) => {
   // 1.fungsi di jika di header kita masukan token atau tidak
@@ -33,7 +33,31 @@ exports.authMiddleware = async (req, res, next) => {
 
   // 3. ambil data user berdasarkan kondisi decoded
   const currentUser = await User.findByPk(decoded.id);
+  if(!currentUser) {
+    return next(
+      res.status(401).json({
+        status: 401,
+        message: "User terhapus token tidak bisa digunakan",
+      })
+    );
+  }
   //   console.log(currentUser);
-  req.user;
+  req.user = currentUser;
   next();
 };
+
+exports.permissionUser = (...roles) => {
+  return async(req, res, next) => {
+    const rolesData = await Role.findByPk(req.user.role_id)
+    const roleName = rolesData.name
+
+    if(!roles.includes(roleName)) {
+      return next(res.status(403).json({
+        status: 403,
+        error: "User dont have a permission"
+      }))
+    }
+
+    next()
+  }
+}
--- BACK END/expressjs/routes/categories.js
+++ BACK END/expressjs/routes/categories.js
@@ -1,23 +1,32 @@
 const express = require("express");
 const router = express.Router();
-const { getAllCategories, storeCategory, detailCategory, updateCategory, deleteCategory } = require("../controllers/categoryController");
-const { authMiddleware} = require("../middleware/UserMiddleware");
+const {
+  getAllCategories,
+  storeCategory,
+  detailCategory,
+  updateCategory,
+  deleteCategory,
+} = require("../controllers/categoryController");
+const {
+  authMiddleware,
+  permissionUser,
+} = require("../middleware/UserMiddleware");
 //Routing
 
 //Get all
-router.get("/", getAllCategories)
+router.get("/", getAllCategories);
 
 //Detail data
-router.get('/:id', authMiddleware, detailCategory)
+router.get("/:id", authMiddleware, detailCategory);
 
 //Update data
-router.put("/:id", updateCategory)
+router.put("/:id", permissionUser("Admin"), updateCategory);
 
 //Create data
-router.post("/", storeCategory)
+router.post("/", permissionUser("Admin"), storeCategory);
 
 //Delete data
-router.delete("/:id", deleteCategory)
+router.delete("/:id", permissionUser("Admin"), deleteCategory);
 
 router.get("/:nama", (req, res) => {
   res.send(`ini endpoint dari route params ${req.params.nama}`);
