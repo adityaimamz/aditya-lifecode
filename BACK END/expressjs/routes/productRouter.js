60ca2508f81b67227e59d10a216b1378c7bbc874
ADITYA IMAM ZUHDI <100418056+adityaimamz@users.noreply.github.com>
2023-10-21 21:11:32 +0700

    add migration

--- BACK END/expressjs/routes/productRouter.js
+++ BACK END/expressjs/routes/productRouter.js
@@ -2,6 +2,10 @@ const express = require('express');
 const router = express.Router();
 const {addProduct, readProduct, detailProduct, updateProduct, deleteProduct} = require('../controllers/productController');
 const {uploadOption} = require("../utils/fileUpload");
+const {
+    authMiddleware,
+    permissionUser,
+  } = require("../middleware/UserMiddleware");
 
 //Tambah Product
 //endpoint = POST api/v1/product/
@@ -17,10 +21,10 @@ router.get("/:id", detailProduct)
 
 // Update Product
 //endpoint = PUT api/v1/product/id
-router.put("/:id", uploadOption.single(`image`), updateProduct);
+router.put("/:id", uploadOption.single(`image`),authMiddleware, permissionUser("Admin"), updateProduct);
 
 // Delete Product
 //endpoint = DELETE api/v1/product/id
-router.delete("/:id", deleteProduct);
+router.delete("/:id",authMiddleware, permissionUser("Admin"), deleteProduct);
 
 module.exports = router;
