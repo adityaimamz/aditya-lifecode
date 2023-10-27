252fea35a39213cd2f160d50a37e52e944086808
ADITYA IMAM ZUHDI <100418056+adityaimamz@users.noreply.github.com>
2023-10-27 21:21:33 +0700

    navbar

--- PROJECTS/Portofolio V2/src/component/Navbar.js
+++ PROJECTS/Portofolio V2/src/component/Navbar.js
@@ -1,12 +1,15 @@
 import React from "react";
 import Link from "next/link";
 import Logo from "./Logo";
+import { useRouter } from "next/router";
 
 const CustomLink = ({ href, title, className = "" }) => {
+  const router = useRouter();
+  ;
   return (
-    <Link href={href} className={`${className} relative`}>
+    <Link href={href} className={`${className} relative group`}>
       {title}
-      <span className="h-[1px] inline-block w-full bg-dark absolute left-0 top-full -mt-0.6"></span>
+      <span className={`h-[2px] inline-block  bg-dark absolute left-0 top-full -mt-0.6 group-hover:w-full transition-[width] ease duration-300 ${router.asPath === href ? 'w-full' : 'w-0'}`}></span>
     </Link>
   );
 };
@@ -16,9 +19,9 @@ const Navbar = () => {
     <header className="h-full px-32 py-8 font-medium flex items-center justify-between">
       <nav>
         <CustomLink href="/" title="Home" className="mr-4" />
-        <CustomLink href="/" title="About" className="mx-4" />
+        <CustomLink href="/About" title="About" className="mx-4" />
         <CustomLink href="/projects" title="Projects" className="mx-4" />
-        <CustomLink href="/" title="Contact" className="ml-4" />
+        <CustomLink href="/Contact" title="Contact" className="ml-4" />
       </nav>
       <nav>
         <Link href="/" target="_blank">
